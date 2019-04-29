6159d808981ab04e5e02659467eda5ee6162d8de
jackiegatwiri <jaciegatz@gmail.com>
2019-04-29 14:39:22 +0300

    add test to check whether an instance can be instantiated correctly

--- src/test/java/RangersTest.java
+++ src/test/java/RangersTest.java
@@ -7,4 +7,22 @@ public class RangersTest {
         Rangers testRanger = new Rangers("Jackie", "Gatwiri", 1);
         assertTrue(testRanger instanceof Rangers);
     }
+
+    @Test
+    public void Rangers_instantiateWithgetFirstName_true(){
+        Rangers testRanger = new Rangers("Jackie", "Gatwiri", 1);
+        assertEquals("Jackie", testRanger.getFirstName());
+    }
+
+    @Test
+    public void Rangers_instantiateWithgetSecondName_true(){
+        Rangers testRanger = new Rangers("Jackie", "Gatwiri", 1);
+        assertEquals("Gatwiri", testRanger.getSecondName());
+    }
+
+    @Test
+    public void Rangers_instantiateWithBadgeNo_true(){
+        Rangers testRanger = new Rangers("Jackie", "Gatwiri", 1);
+        assertEquals(1, testRanger.getBadgeNo());
+    }
 }
