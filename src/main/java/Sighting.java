c0eb711bdacdd116d9df0cfca0f8dbb77ae9c82c
jackiegatwiri <jaciegatz@gmail.com>
2019-04-30 17:42:28 +0300

    add all, find, and save method to the Sightings class to store, retrieve and find any instance of the sighting from the database

--- src/main/java/Sighting.java
+++ src/main/java/Sighting.java
@@ -6,6 +6,7 @@ public class Sighting {
     private int animalId;
     private String location;
     private String rangerName;
+    private int id;
 
     public Sighting(int animalId, String location, String rangerName) {
         this.animalId = animalId;
@@ -24,43 +25,52 @@ public class Sighting {
     public String getRangerName() {
         return rangerName;
     }
+    public int getId() {
+        return id;
+    }
 
-//    @Override
-//    public boolean equals(Object otherAnimal){
-//        if(!(otherAnimal instanceof Animals)){
-//            return false;
-//        }
-//        else {
-//            Animals newAnimals = (Animals) otherAnimal;
-//            return this.getName().equals(newAnimals.getName());
-//        }
-//    }
-//    public void save(){
-//        try(Connection con = DB.sql2o.open()){
-//            String sql = "INSERT INTO animals(name, type) VALUES (:name, :type)";
-//            this.id = (int) con.createQuery(sql, true)
-//                    .addParameter("name", this.name)
-//                    .addParameter("type", TYPE)
-//                    .executeUpdate()
-//                    .getKey();
-//        }
-//    }
-//    public static List<Animals> all(){
-//        String sql = "SELECT * FROM animals where type = 'Not Endangered'";
-//        try(Connection con = DB.sql2o.open()){
-//            return con.createQuery(sql)
-//                    .throwOnMappingFailure(false) //throw an exception to tell database not to return the fiels that it is not using
-//                    .executeAndFetch(Animals.class);
-//        }
-//
-//    }
-//    public static Animals find(int id) {
-//        try(Connection con = DB.sql2o.open()) {
-//            String sql = "SELECT * FROM animals where id=:id";
-//            Animals animal = con.createQuery(sql)
-//                    .addParameter("id", id)
-//                    .executeAndFetchFirst(Animals.class);
-//            return animal;
-//        }
-//    }
+    @Override
+    public boolean equals(Object otherSighting){
+        if(!(otherSighting instanceof Sighting)){
+            return false;
+        }
+        else {
+            Sighting newSighting = (Sighting) otherSighting;
+            return this.id == newSighting.getId();
+        }
+
+//        this.getAnimalId() == (newSighting.getAnimalId()) &&
+//                this.getLocation().equals(newSighting.getLocation()) &&
+//                this.getRangerName().equals(newSighting.getLocation());
+    }
+    public void save(){
+        try(Connection con = DB.sql2o.open()){
+            String sql = "INSERT INTO sightings(animalid, location, rangername) VALUES (:animalid, :location, :rangername)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("animalid", this.animalId)
+                    .addParameter("location", this.location)
+                    .addParameter("rangername", this.rangerName)
+                    .executeUpdate()
+                    .getKey();
+        }
+    }
+    public static List<Sighting> allSighting(){
+        String sql = "SELECT * FROM sightings";
+        try(Connection con = DB.sql2o.open()){
+            return con.createQuery(sql)
+                    .executeAndFetch(Sighting.class);
+        }
+
+    }
+    public static Sighting find(int id) {
+        try(Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM sightings where id=:id";
+            Sighting sighting = con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Sighting.class);
+            return sighting;
+        }
+    }
+
+
 }
--- src/test/java/SightingTest.java
+++ src/test/java/SightingTest.java
@@ -1,2 +1,70 @@
+import org.junit.Rule;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+
 public class SightingTest {
+
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void Sighting_intantiatesCorrectly_true(){
+        Sighting testSighting = new Sighting(1, "kenya", "mark");
+        assertEquals(true, testSighting instanceof Sighting);
+    }
+    @Test
+    public void Sighting_instantiatesWithAnimalId_String() {
+        Sighting testSighting = new Sighting(1, "kenya", "mark");
+        assertEquals(1, testSighting.getAnimalId());
+    }
+    @Test
+    public void Sighting_instantiatesWithLocation_String() {
+        Sighting testSighting = new Sighting(1, "kenya", "mark");
+        assertEquals("kenya", testSighting.getLocation());
+    }
+    @Test
+    public void Sighting_instantiatesWithRangerNAme_String() {
+        Sighting testSighting = new Sighting(1, "kenya", "mark");
+        assertEquals("mark", testSighting.getRangerName());
+    }
+    @Test
+    public void equals_returnsTrueIfTheNamesAreEqual_true(){
+        Sighting firstSighting = new Sighting(1, "kenya", "mark");
+        Sighting secondSighting = new Sighting(1, "kenya", "mark");
+        assertTrue(firstSighting.equals(secondSighting));
+    }
+    @Test
+    public void save_saveInstanceToTheDatabase(){
+        Sighting testSighting = new Sighting(1, "kenya", "mark");
+        testSighting.save();
+        assertEquals(true, Sighting.allSighting().get(0).equals(testSighting));
+    }
+    @Test
+    public void save_assignsIdToAnimals() {
+        Sighting testSighting = new Sighting(1, "kenya", "mark");
+        testSighting.save();
+        Sighting savedSighting = Sighting.allSighting().get(0);
+        assertEquals(savedSighting.getId(), testSighting.getId());
+    }
+    @Test
+    public void all_returnsAllInstancesOfAnimals_true() {
+        Sighting testSighting = new Sighting(1, "kenya", "mark");
+        testSighting.save();
+        Sighting testSighting2 = new Sighting(1, "kenya", "mark");
+        testSighting2.save();
+        assertEquals(true, Sighting.allSighting().get(0).equals(testSighting));
+        assertEquals(true, Sighting.allSighting().get(1).equals(testSighting2));
+    }
+    @Test
+    public void find_returnsAnimalWithSameId_secondAnimal() {
+        Sighting testSighting = new Sighting(1, "kenya", "mark");
+        testSighting.save();
+        Sighting testSighting2 = new Sighting(1, "kenya", "mark");
+        testSighting2.save();
+        assertEquals(Sighting.find(testSighting.getId()), testSighting);
+    }
+
+
 }
